5ea045729973dd3138779dfd49a97ce5a629787e
Dave Tan <davetankx@gmail.com>
2025-11-02 20:42:29 +0800

    add links to blog and resume

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -14,10 +14,13 @@
         <div class="font-bold text-6xl">Hi, I'm <span class="text-tame-green">Dave Tan</span></div>
         <div class="pt-12  text-lg font-medium max-w-3xl">I am currently serving my national service as a Cyber Defence Operator in the Digital Intelligence Service, where I am gaining hands-on experience in the cybersecurity landscape. My training  has built upon my solid foundation in networking and basic  cybersecurity.<br /><br />In my free time, I pursue my passion for programming, often participating in Capture-the-Flag (CTF) competitions  to sharpen and test my technical skills.<br /><br />I am particularly drawn to solving complex problems—a skill that transcends domains, which has  guided me through my previous experiences.<br /><br />After completing my national service, I intend to pursue Computer Science in university</div>
         <div class="pt-14">
-          <button class="bg-tame-green text-neutral-50 pl-4 pr-10 py-2 text-xl flex items-center gap-3 font-semibold">
-            <img src="../../public/arrow-right-fill.svg" alt="Arrow icon" class="h-12 w-12">
+        <a href="/resume" target="_blank" rel="noopener noreferrer">
+          <button className="bg-tame-green text-neutral-50 pl-4 pr-10 py-2 text-xl flex items-center gap-3 font-semibold">
+            <img src="/arrow-right-fill.svg" alt="Arrow icon" className="h-12 w-12" />
             See my resume
           </button>
+        </a>
+
 
         </div>
       </div>
--- src/views/Writeups.vue
+++ src/views/Writeups.vue
@@ -9,33 +9,33 @@ import WriteupInfocard from '../components/WriteupInfocard.vue';
 
 <template>
   <div class="min-h-[calc(100vh-100px)] max-sm:min-h-[calc(100vh-56px)] flex flex-col pl-40 pt-12 pr-16 bg-muted-plum">
-    <div class="flex flex-row">
-      <div class="basis-1/2 ">
+    <div class="flex flex-row gap-16">
+      <div class="basis-1/2">
         <div class="font-semibold text-4xl">Write-ups</div>
-        <div class="pt-12  text-lg font-medium max-w-30%">I love solving problems. <br /><br />I have been participating in CTF competitions since 2022 as one of the three members of TOP PROGRAMMER 2005 DATABASE MAESTRO.<br /><br />Here's some of my write-ups</div>
-        <div class="pt-16 flex flex-row gap-12">
-          <a href="https://top-programmer-2005-database-maestro.surge.sh/">
+        <div class="pt-12 text-lg font-medium ">I love solving problems. <br /><br />I have been participating in CTF competitions since 2022 as one of the three members of TOP PROGRAMMER 2005 DATABASE MAESTRO.<br /><br /></div>
+        <div class="flex flex-col">
+          <a class="text-lg" href="/blog"> Here are some write-ups on my <u>blog</u></a>
+          <a class="mt-16" href="https://top-programmer-2005-database-maestro.surge.sh/">
             <button class="bg-tp2dm-savannah pl-4 pr-6 py-2 text-lg flex items-center gap-3  text-neutral-950 w-80 font-light">
               <img src="../../public/giraffe_head.png" alt="TP2DM Logo" class="h-12 w-12">
               official TP2DM web page
             </button>
           </a>
-          <a href="https://ctftime.org/user/152999">
-            
+          <a class="mt-8" href="https://ctftime.org/user/152999">
             <button class="bg-[#ff8981] pl-4 pr-6 py-2 text-lg flex items-center gap-4 text-neutral-950 w-80 font-light">
-              <img src="../../public/CTFtime.png" alt="CTFtime Logo" class="h-10 w-12">
+              <img src="../../public/CTFtime.png" alt="CTFtime Logo" class="h-12 w-12">
               CTFtime/Fasnon
             </button>
           </a>
-
         </div>
       </div>
-      <div class="min-w-20" />
-      <div class="flex flex-col w-full">
+      <div class="basis-1/2 flex flex-col">
         <p>Recent</p>
         <WriteupInfocard />
         <p>My Favourites</p>
+        <WriteupInfocard />
         <p>Random</p>
+        <WriteupInfocard />
       </div>
     </div>
   </div>
